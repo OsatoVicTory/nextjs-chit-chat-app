b55b862926692ec403dcce1ce4b29f8931b101aa
Osato <osatohanmenogbeide1@gmail.com>
2025-05-20 16:01:09 +0100

    ignore errors during build (eslint)

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
 };
 
 export default nextConfig;
