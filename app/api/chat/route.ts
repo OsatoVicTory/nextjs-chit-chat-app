e360072512d6c2d0321af2b49d3c2b1dd37ac9db
Osato <osatohanmenogbeide1@gmail.com>
2025-05-27 11:20:51 +0100

    fixed path

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -7,14 +7,14 @@ interface DBUserType extends UserType {
 };
 
 export async function GET() {
-    const data_ = fs.readFileSync("./app/api/chat/db.json", "utf-8");
+    const data_ = fs.readFileSync(process.cwd() + "/app/api/chat/db.json", "utf-8");
     const db = JSON.parse(data_);
     return NextResponse.json({ data: db }, { status: 200 });
 };
 
 export async function POST(req: NextRequest) {
     const data : DBUserType = await req.json();
-    const data_ = fs.readFileSync("./app/api/chat/db.json", "utf-8");
+    const data_ = fs.readFileSync(process.cwd() + "/app/api/chat/db.json", "utf-8");
     const db = JSON.parse(data_);
     const id = data.cache_id;
     const chatData = { 
@@ -32,14 +32,14 @@ export async function POST(req: NextRequest) {
         } 
     }
     if(!seen) db.push(chatData);
-    fs.writeFileSync("./app/api/chat/db.json", JSON.stringify(db, null, 2), "utf-8");
+    fs.writeFileSync(process.cwd() + "/app/api/chat/db.json", JSON.stringify(db, null, 2), "utf-8");
     return NextResponse.json({ data: { status: "success" } }, { status: 200 });
 };
 
 export async function PATCH(req: NextRequest) {
     const data : UserType = await req.json();
     let cnt = 0;
-    const data_ = fs.readFileSync("./app/api/chat/db.json", "utf-8");
+    const data_ = fs.readFileSync(process.cwd() + "/app/api/chat/db.json", "utf-8");
     const db : ChatListType[] = JSON.parse(data_);
     const newDb : ChatListType[] = [];
     db.forEach(_user => {
@@ -49,6 +49,6 @@ export async function PATCH(req: NextRequest) {
             newDb.push({ ..._user, _i: cnt++, lastSeen: data.lastSeen });
         }
     });
-    fs.writeFileSync("./app/api/chat/db.json", JSON.stringify(newDb, null, 2), "utf-8");
+    fs.writeFileSync(process.cwd() + "/app/api/chat/db.json", JSON.stringify(newDb, null, 2), "utf-8");
     return NextResponse.json({ data: { status: "success" } }, { status: 200 });
 };
